f9ce4693ee3554878147c8fd1cac1a2e659ec2c7
Vincent Weber <vincent.weber@fernuni.ch>
2025-02-01 16:12:07 +0100

    01.02.25

--- task/Matlab Scripts/online/create_total_table.m
+++
@@ -1,9 +0,0 @@
-table1 = readtable('/Users/vincentweber/OneDrive - FernUni Schweiz/Physical Activity and Memory/task/Matlab Scripts/online/final_table_vweber.csv');
-table2 = readtable('/Users/vincentweber/OneDrive - FernUni Schweiz/Physical Activity and Memory/task/Matlab Scripts/online/final_table_fink.csv');
-table3 = readtable('/Users/vincentweber/OneDrive - FernUni Schweiz/Physical Activity and Memory/task/Matlab Scripts/online/final_table_liechti.csv');
-table4 = readtable('/Users/vincentweber/OneDrive - FernUni Schweiz/Physical Activity and Memory/task/Matlab Scripts/online/final_table_aberisha.csv');
-table5 = readtable('/Users/vincentweber/OneDrive - FernUni Schweiz/Physical Activity and Memory/task/Matlab Scripts/online/final_table_jbanninger.csv');
-
-total_table = [table1;table2;table3;table4;table5];
-
-writetable(total_table,'total_table.csv')
